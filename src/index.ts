8fc3e51c119db22b765c8f78c36a3c673112b7c2
Clyde D'Souza <clyd94@gmail.com>
2023-12-27 00:53:33 +1300

    Add consumer, add index file for package

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { GetZodiacSign, GetZodiacSignForDate, getAllZodiacSigns } from "./Zodiac"
+import { getZodiacSign } from "./ChineseZodiac"
+
+export default { GetZodiacSign, GetZodiacSignForDate, getAllZodiacSigns, getZodiacSign }
